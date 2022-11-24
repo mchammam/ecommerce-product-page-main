a7524ae3e98f1a240a4e07507ab003193b3c4cfb
Mohammed Chammam <mohammed.chammam@gmail.com>
2022-11-24 21:05:52 +0100

    ArticleItems now fetched from API

--- src/components/Articles.js
+++ src/components/Articles.js
@@ -1,7 +1,17 @@
-import articleItems from '../data/articles.json';
+import { useEffect, useState } from 'react';
 import Article from './Article';
 
+//import articleItems from '../data/articles.json'
+
 function Articles() {
+    const [articleItems, setArticleItems] = useState([])
+    useEffect(async () => {
+        const res = await fetch("http://127.0.0.1:2000/articles/")//.catch()
+        const jsonRes = await res.json() 
+        setArticleItems(jsonRes.data)
+    }, [])
+
+    //console.log(articleItems2)
     return (
         <>
             {articleItems.map((articleData) => (
