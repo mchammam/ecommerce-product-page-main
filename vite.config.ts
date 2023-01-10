711bca98b59227344608841c6655474dee167195
Mohammed Chammam <mohammed.chammam@gmail.com>
2023-01-10 11:42:48 +0100

    Configs for GitHub pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/ecommerce-product-page-main/',
   build: {
     outDir: 'build'
   }
